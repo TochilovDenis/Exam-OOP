48446917e360e4a0d776b6bfcbfeaf828cb55488
Денис Точилов <denis.tochilov.1990@gmail.com>
2024-04-04 12:17:15 +0300

    Добавление класса FileManager для управления содержимым директорий и поиска файлов
    - Класс FileManager предоставляет функциональность для получения содержимого директории, отображения содержимого директории и поиска файлов по маске в директории и ее поддиректориях.
    - Метод `getDirectoryContents` возвращает список путей к файлам и директориям в указанной директории.
    - Метод `displayDirectoryContents` отображает содержимое директории, вызывая `getDirectoryContents` и печатая каждый элемент списка.
    - Метод `matchesMask` проверяет, соответствует ли имя файла или директории заданной маске, используя метод `find` для поиска подстроки в имени.
    - Метод `SearchFiles` выполняет рекурсивный поиск файлов в директории и ее поддиректориях, используя `matchesMask` для фильтрации файлов по маске.
    
    Этот коммит улучшает систему управления файлами, добавляя возможность просмотра содержимого директорий и поиска файлов по маске. Это позволяет легко искать и управлять файлами в файловой системе, упрощая работу с большими объемами данных.
    
    Ограничения:
    - Поиск файлов по маске может быть неэффективным для больших директорий из-за рекурсивного обхода всех поддиректорий.
    - Обработка ошибок и специфические поведения файловой системы могут быть улучшены для обеспечения более надежной работы с файлами и директориями.
    
    Этот коммит улучшает систему управления файлами, добавляя функциональность поиска и отображения содержимого директорий, что упрощает работу с файлами и директориями.

---
+++ File Manager/Filemanadger.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include "Directory.h"
+
+class FileManager {
+public:
+    vector<string> getDirectoryContents(const string& path) {
+        vector<string> contents;
+        for (const auto& entry : fs::directory_iterator(path)) {
+            contents.push_back(entry.path().string());
+        }
+        return contents;
+    }
+
+    void displayDirectoryContents(const string& path) {
+        vector<string> contents = getDirectoryContents(path);
+        for (const auto& item : contents) {
+            cout << item << endl;
+        }
+    }
+
+    bool matchesMask(const fs::path& path, const string& mask) {
+        return path.filename().string().find(mask) != string::npos;
+    }
+
+    void SearchFiles(const fs::path& dirPath, const std::string& mask) {
+        for (const auto& entry : fs::recursive_directory_iterator(dirPath)) {
+            if (entry.is_regular_file() && matchesMask(entry.path(), mask)) {
+                cout << entry.path() << endl;
+            }
+            else {
+                cout << "Файл или папку не найден!" << endl;
+            }
+        }
+    }
+};
