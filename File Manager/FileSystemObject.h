df28c8d8a176d71e7437b9b9dfe8dbd5c9ca4550
Денис Точилов <denis.tochilov.1990@gmail.com>
2024-04-04 12:04:03 +0300

    Добавление абстрактного класса FileSystemObject для операций с файловой системой
    - Введение класса FileSystemObject с чистыми виртуальными методами для операций с файловой системой, такими как Create, Delete, Rename, Copy, Move и ShowSize.
    - Использование библиотеки C++17 filesystem для работы с путями и операциями.
    - Определение псевдонима пространства имен для std::filesystem для упрощения кода.
    
    Этот класс служит основой для реализации конкретных операций с файловой системой, предоставляя четкий интерфейс для манипуляций с файлами и директориями. Он направлен на абстракцию специфических деталей операций с файловой системой, облегчая тестирование и расширение функциональности, связанной с файловой системой.
    
    Ограничения:
    - Текущая реализация является абстрактной и требует конкретных подклассов для выполнения фактических операций с файловой системой.
    - Обработка ошибок и специфические поведения файловой системы не реализованы в этом базовом классе.
    
    Этот коммит закладывает основу для более надежной системы управления файловой системой в приложении.

---
+++ File Manager/FileSystemObject.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <cstdint>
+#include <string>
+using namespace std;
+namespace fs = std::filesystem;
+
+class FileSystemObject {
+public:
+	virtual ~FileSystemObject() {}
+	virtual void Create(const string& path) = 0;
+	virtual void Delete(const string& path) = 0;
+	virtual void Rename(const string& oldPath, const string& newPath) = 0;
+	virtual void Copy(const string& sourcePath, const string& destinationPath) = 0;
+	virtual void Move(const string& oldPath, const string& newPath) = 0;
+	virtual void ShowSize(const string& path) = 0;
+};
