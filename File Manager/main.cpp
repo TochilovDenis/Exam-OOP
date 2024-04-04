d90ac7e6e7730f2b3271ef568a85b02fe37320f6
Денис Точилов <denis.tochilov.1990@gmail.com>
2024-04-04 12:20:26 +0300

    Добавление демонстрационного приложения для работы с файлами и директориями
    - Включает в себя демонстрацию создания, переименования, копирования, перемещения, отображения размера файлов и директорий, а также поиска файлов по маске.
    - Использует классы File, Directory и FileManager для выполнения различных операций с файловой системой.
    - Примеры демонстрируют базовые операции с файлами и директориями, включая создание и удаление, а также более сложные операции, такие как поиск файлов по маске.
    
    Этот коммит представляет собой демонстрационное приложение, которое демонстрирует возможности работы с файловой системой, реализованные в предыдущих коммитах. Он служит как пример использования абстрактного интерфейса FileSystemObject и его конкретных реализаций для управления файлами и директориями.
    
    Ограничения:
    - Приложение зависит от конкретных путей к файлам и директориям, что может ограничивать его переносимость и использование в различных средах.
    - Обработка ошибок и специфические поведения файловой системы могут быть улучшены для обеспечения более надежной работы с файлами и директориями.
    
    Этот коммит улучшает систему управления файлами, добавляя демонстрационное приложение, которое демонстрирует основные возможности работы с файловой системой.

---
+++ File Manager/main.cpp
@@ -0,0 +1,57 @@
+﻿#include "File.h"
+#include "Directory.h"
+#include "Filemanadger.h"
+
+int main() {
+    setlocale(0, "");
+
+    File file;
+    Directory dir;
+
+    cout << "\nСоздаем папку и файл:\n";
+    dir.Create("D:\\FileManadger\\example\\example");
+    dir.Create("D:\\FileManadger\\example\\example3");
+    file.Create("D:\\FileManadger\\example\\example\\example.txt");
+
+    cout << "\nПереименования папки и файла:\n";
+    dir.Rename("D:\\FileManadger\\example\\example", "D:\\FileManadger\\example\\example1");
+    file.Rename("D:\\FileManadger\\example\\example1\\example.txt", "D:\\FileManadger\\example\\example1\\example1.txt");
+
+    cout << "\nКопирование папку и файл:\n";
+    dir.Copy("D:\\FileManadger\\example\\example1", "D:\\FileManadger\\example\\example2");
+    file.Copy("D:\\FileManadger\\example\\example1\\example1.txt", "D:\\FileManadger\\example\\example1\\example2.txt");
+
+    cout << "\nПеремещение папку и файл:\n";
+    dir.Move("D:\\FileManadger\\example\\example3", "D:\\FileManadger\\example\\example1\\example3");
+    file.Move("D:\\FileManadger\\example\\example2\\example1.txt", "D:\\FileManadger\\example\\example1\\example2.txt");
+
+    cout << "\nРазмер папки и файла:\n";
+    dir.ShowSize("D:\\FileManadger");
+    file.ShowSize("D:\\FileManadger\\example\\example1\\example2.txt");
+
+    FileManager fileManager;
+    cout << "\nПоказываем содержимое дисков:\n";
+    string path = "D:\\FileManadger\\example";
+
+    fileManager.displayDirectoryContents(path);
+    cout << "\nПоиск папку и файла:\n";
+    fileManager.SearchFiles("D:\\FileManadger", "example2.txt");
+
+    cout << "\nУдаляем папку и файл:\n";
+    dir.Delete("D:\\FileManadger\\example");
+
+    return 0;
+}
+
+/*
+Первое задание
+Реализовать простейший файловый менеджер с использованием ООП (классы, наследование и так далее).
+Файловый менеджер должен иметь такие возможности:
+    1. показывать содержимое дисков;
+    2. создавать папки\\файлы;
+    3. удалять папки\\файлы;
+    4. переименовывать папки\\файлы;
+    5. копировать\\переносить папки\\файлы;
+    6. вычислять размер папки\\файла;
+    7. производить поиск по маске (с поиском по подпапкам) и так далее.
+*/
