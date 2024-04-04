996ede0c33af2396fa6201deb68727fa3d0cefc4
Денис Точилов <denis.tochilov.1990@gmail.com>
2024-04-04 12:08:09 +0300

    Реализация класса File для работы с файлами через абстрактный интерфейс FileSystemObject
    - Класс File реализует абстрактный класс FileSystemObject, предоставляя конкретные реализации методов для создания, удаления, переименования, копирования, перемещения файлов и отображения их размера.
    - Использует стандартные функции C++ для работы с файлами и директориями, включая создание файлов, удаление файлов, переименование файлов, копирование файлов, перемещение файлов и получение размера файлов.
    - Обработка ошибок выполняется через блоки try-catch, позволяя корректно обрабатывать исключения, связанные с файловой системой.
    
    Этот коммит расширяет базовую функциональность, предоставленную абстрактным классом FileSystemObject, добавляя конкретные реализации для работы с файлами. Это позволяет легко расширять систему управления файлами, добавляя новые типы объектов файловой системы, такие как директории или ссылки.
    
    Ограничения:
    - Реализация предполагает, что все операции с файлами успешно выполняются. В реальных условиях могут возникать различные исключения, которые необходимо корректно обрабатывать.
    - Обработка ошибок и специфические поведения файловой системы могут быть улучшены для обеспечения более надежной работы с файлами.
    
    Этот коммит улучшает систему управления файлами, добавляя возможность работы с файлами через абстрактный интерфейс, что упрощает тестирование и расширение функциональности.

---
+++ File Manager/File.h
@@ -0,0 +1,56 @@
+﻿#pragma once
+#include "FileSystemObject.h"
+
+class File : public FileSystemObject {
+public:
+	void Create(const string& path) {
+		ofstream ofs(path);
+		if (ofs) {
+			ofs << "Это текст в новом файле.\n";
+			ofs.close();
+			cout << "Файл_ " << path << " _создан." << endl;
+		}
+		else {
+			cout << "Не удалось создать файл " << path << endl;
+		}
+	}
+	void Delete(const string& path) {
+		if (fs::remove(path)) {
+			cout << "Файл_ " << path << " _успешно удален." << endl;
+		}
+		else {
+			cout << "Не удалось удалить файл " << path << endl;
+		}
+	}
+	void Rename(const string& oldPath, const string& newPath) {
+		try {
+			fs::rename(oldPath, newPath);
+			cout << "Файл " << newPath << " переименован успешно." << endl;
+		}
+		catch (fs::filesystem_error& e) {
+			cout << "Ошибка при переименовании файла: " << e.what() << endl;
+		}
+	}
+	void Copy(const string& sourcePath, const string& destinationPath) {
+		try {
+			fs::copy(sourcePath, destinationPath, fs::copy_options::overwrite_existing);
+			cout << "Файл " << destinationPath << " успешно скопирован." << endl;
+		}
+		catch (fs::filesystem_error& e) {
+			cout << "Ошибка при копировании файла: " << e.what() << endl;
+		}
+	}
+	void Move(const string& oldPath, const string& newPath) {
+		try {
+			fs::rename(oldPath, newPath);
+			std::cout << "Файл " << newPath << "успешно перемещен." << std::endl;
+		}
+		catch (fs::filesystem_error& e) {
+			std::cout << "Ошибка при перемещении файла: " << e.what() << std::endl;
+		}
+	}
+	void ShowSize(const string& path) {
+		uintmax_t fileSize = fs::file_size(path);
+		cout << "Размер файла: " << fileSize << " байт" << endl;
+	}
+};
