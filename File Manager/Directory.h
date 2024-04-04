8d858651ed2b8267f868ea0e40b1af3b9f960a13
Денис Точилов <denis.tochilov.1990@gmail.com>
2024-04-04 12:10:53 +0300

    Добавление класса Directory для работы с директориями через абстрактный интерфейс FileSystemObject
    - Класс Directory реализует абстрактный класс FileSystemObject, предоставляя конкретные реализации методов для создания, удаления, переименования, копирования, перемещения директорий и отображения их размера.
    - Использует стандартные функции C++ для работы с файловой системой, включая создание директорий, удаление директорий и их содержимого, переименование директорий, копирование директорий и перемещение директорий.
    - Реализована функция для вычисления общего размера директории, включая все файлы внутри нее, используя рекурсивный итератор по директории.
    - Обработка ошибок выполняется через блоки try-catch, позволяя корректно обрабатывать исключения, связанные с файловой системой.
    
    Этот коммит расширяет базовую функциональность, предоставленную абстрактным классом FileSystemObject, добавляя конкретные реализации для работы с директориями. Это позволяет легко расширять систему управления файлами, добавляя новые типы объектов файловой системы, такие как файлы или ссылки.
    
    Ограничения:
    - Реализация предполагает, что все операции с директориями успешно выполняются. В реальных условиях могут возникать различные исключения, которые необходимо корректно обрабатывать.
    - Обработка ошибок и специфические поведения файловой системы могут быть улучшены для обеспечения более надежной работы с директориями.
    
    Этот коммит улучшает систему управления файлами, добавляя возможность работы с директориями через абстрактный интерфейс, что упрощает тестирование и расширение функциональности.

---
+++ File Manager/Directory.h
@@ -0,0 +1,63 @@
+﻿#pragma once
+#include "File.h"
+#include <string>
+#include <vector>
+using namespace std;
+
+class Directory : public FileSystemObject {
+public:
+	void Create(const string& path) {
+		if (!fs::exists(path)) {
+			fs::create_directories(path);
+			cout << "Директория_ " << path << " _создана." << endl;
+		}
+		else {
+			cout << "Директория_ " << path << " _уже существует." << endl;
+		}
+	}
+	void Delete(const string& path) {
+		uintmax_t n = fs::remove_all(path);
+		cout << "Удалено_ " << n << " _файлов или директорий" << endl;
+	}
+	void Rename(const string& oldPath, const string& newPath) {
+		try {
+			fs::rename(oldPath, newPath);
+			cout << "Директория переименована " << newPath << " успешно." << endl;
+		}
+		catch (fs::filesystem_error& e) {
+			cout << "Ошибка при переименовании директории: " << e.what() << endl;
+		}
+	}
+	void Copy(const string& sourcePath, const string& destinationPath) {
+		try {
+			fs::copy(sourcePath, destinationPath, fs::copy_options::recursive);
+			cout << "Директория успешно " << destinationPath << " скопирована." << endl;
+		}
+		catch (fs::filesystem_error& e) {
+			cout << "Ошибка при копировании директории: " << e.what() << endl;
+		}
+
+	}
+	void Move(const string& oldPath, const string& newPath) {
+		try {
+			fs::rename(oldPath, newPath);
+			cout << "Директория успешно " << newPath << " перемещена." << endl;
+		}
+		catch (fs::filesystem_error& e) {
+			cout << "Ошибка при перемещении директории: " << e.what() << endl;
+		}
+	}
+	uintmax_t getDirectorySize(const fs::path& path) {
+		uintmax_t totalSize = 0;
+		for (const auto& entry : fs::recursive_directory_iterator(path)) {
+			if (entry.is_regular_file()) {
+				totalSize += entry.file_size();
+			}
+		}
+		return totalSize;
+	}
+	void ShowSize(const string& path) {
+		uintmax_t dirSize = getDirectorySize(path);
+		cout << "Общий размер директории: " << dirSize << " байт" << endl;
+	}
+};
